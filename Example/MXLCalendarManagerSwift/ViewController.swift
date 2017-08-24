e57034a9511d968d0f9c8c3993b8d1189393263d
Ramon Vasconcelos <ramon.vasconcelos@starcut.com>
2017-08-24 14:15:37 +0300

    Initial Files

---
+++ Example/MXLCalendarManagerSwift/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MXLCalendarManagerSwift
+//
+//  Created by ramonstarcut on 08/22/2017.
+//  Copyright (c) 2017 ramonstarcut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
