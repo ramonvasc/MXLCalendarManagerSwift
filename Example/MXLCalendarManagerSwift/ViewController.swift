efcdd5a5a29339d6e3274834491371aabdedcd48
Ramon Vasconcelos <ramonvasc@gmail.com>
2017-08-30 14:25:53 +0300

    Preparing example

--- Example/MXLCalendarManagerSwift/ViewController.swift
+++ Example/MXLCalendarManagerSwift/ViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 import MXLCalendarManagerSwift
 
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
